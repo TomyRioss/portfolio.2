5bfc7dd59c2cdd70e81f2528ff2b1e2fd073dccf
tomy <tomyrios2006@gmail.com>
2024-12-10 15:08:07 -0300

    title created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import NavBar from './components/NavBar';
 import './index.css';
+import NameTitle from './components/NameTitle';
 
 function App() {
   return (
     <>
       <NavBar />
+      <NameTitle />
     </>
   );
 }
---
+++ src/components/NameTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NameTitle = () => {
+  return (
+    <div>
+      <h1 className="text-white">Tomas Rios</h1>
+      <h2>FrontEnd Developer</h2>
+    </div>
+  );
+};
+
+export default NameTitle;
